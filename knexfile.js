4cc24fae038a2f23e02b543b1a35cfe34e7cfeac
Leonel <leonelaf12@gmail.com>
2019-06-23 12:10:38 -0400

    made production key on knexfile so i could deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,17 @@ module.exports = {
     },
 
   },
+
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
   
 };
