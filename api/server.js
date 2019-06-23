4dac72f8b8bc0fc02dd740d924709faeb658d176
Leonel <leonelaf12@gmail.com>
2019-06-22 21:16:24 -0400

    made my users seeds and also made router for auth. crud operations working so far

--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
+
 
 server.get('/', (req, res) => {
     res.send("It's working!")
