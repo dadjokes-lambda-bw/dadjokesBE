dc923553ecf8ce973275f7d6c4f7dafa0642d9e7
Leonel <leonelaf12@gmail.com>
2019-06-27 13:16:49 -0400

    fixing small bugs in routes

---
+++ database/seeds/003-private-jokes.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('privateJokes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('privateJokes').insert([
+        { joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.", },
+        { joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",},
+        { joke: "Why didn’t the skeleton cross the road? Because he had no guts.",},
+        { joke: "What did one nut say as he chased another nut?  I'm a cashew!", }
+      ]);
+    });
+};
